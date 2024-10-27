6f48e46b2cc872401565233832a9dcaf7546bb36
Sanjay Gawai <sanjaygawai1002@gmail.com>
2024-10-27 14:04:16 +0530

    load selected image on bootstrap modal

---
+++ public/index.js
@@ -0,0 +1,31 @@
+// $(document).ready(function () {
+//     // Show loader when the page starts loading
+//     $('#loader-wrapper').show();
+//     // Hide loader and show content when page is fully loaded
+//     $(window).on('load', function () {
+//         $('#loader-wrapper').fadeOut('slow', function () {
+//             $('#content').fadeIn('slow');
+//         });
+//     });
+// });
+
+$(document).ready(function () {
+    const base_url = 'file:///C:/local-project/poster-maker/mobile-layout';
+
+    $('.btn-poster').on('click', function () {
+        const imgSrc = $(this).attr('src');
+        // Show the modal
+        $('#loadPoster').modal('show');
+
+        // Set the image source in the modal preview
+        $('#posterPreview').attr('src', imgSrc);
+
+        // Set the download link and file name
+        const base_url = 'https://status247.netlify.app/mobile-layout/'; // Change this to your base URL
+        $('#btnPosterDownload').attr('download', imgSrc.split('/').pop());
+        $('#btnPosterDownload').attr('href', base_url + imgSrc);
+
+
+    })
+})
+
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -199,6 +199,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
